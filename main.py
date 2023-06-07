86f4ff51baab9fcc858a5f960721d3ac353d29e5
Ved Vekhande <202111087@diu.iiitvadodara.ac.in>
2023-06-08 01:44:22 +0530

    Updated data

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ documents = SimpleDirectoryReader(r'D:\NLP\practice\data').load_data()
 index = VectorStoreIndex.from_documents(documents)
 
 query_engine = index.as_query_engine()
-response = query_engine.query("What is the open Cutoff for Indian Institute of Technology Bhubaneshwar  Civil Engineering ?")
+response = query_engine.query("Name of all Institutes?")
 print(response)
