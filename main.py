e64567c3147087a3b134e0661fc18eec0f37518c
Ved Vekhande <202111087@diu.iiitvadodara.ac.in>
2023-06-09 23:21:47 +0530

    minor

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ documents = SimpleDirectoryReader(r'D:\NLP\practice\data').load_data()
 index = VectorStoreIndex.from_documents(documents)
 
 query_engine = index.as_query_engine()
-response = query_engine.query("Name of all Institutes?")
+response = query_engine.query("Name of all Institutes in pdf?")
 print(response)
