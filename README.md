e4c842c8a9977cdf8ca8e85f3278991e6b444ce8
Ved Vekhande <202111087@diu.iiitvadodara.ac.in>
2023-06-21 14:40:25 +0530

    updated

--- README.md
+++
@@ -1,1 +0,0 @@
-Pratice files for AI bot using data from given PDFs, excel, etc.
--- app.py
+++ app.py
@@ -5,12 +5,13 @@ from llama_index.query_engine import RetrieverQueryEngine
 from llama_index.indices.postprocessor import SimilarityPostprocessor
 
 from llama_index import SimpleDirectoryReader
+import time
 
 documents = SimpleDirectoryReader('D:/NLP/practice/data').load_data()
-
+print("4")
 # build index
 index = GPTKeywordTableIndex.from_documents(documents)
-
+print("5")
 # configure retriever
 retriever = index.as_retriever(retriever_mode="default")
 
@@ -18,15 +19,17 @@ retriever = index.as_retriever(retriever_mode="default")
 response_synthesizer = ResponseSynthesizer.from_args(
     node_postprocessors=[
     SimilarityPostprocessor(similarity_cutoff=0.7)
-   ]
+   ],
+    response_mode="compact"
 )
-
-query_engine = RetrieverQueryEngine(retriever, response_mode= 'compact')
-response = query_engine.query("What was the tenure of Dr. kalam?")
-
+print("3")
+query_engine = RetrieverQueryEngine(retriever)
+response = query_engine.query("IIT Madras Aerospace Engineering closing rank for Open category Gender Neutral?")
+print("2")
 # get response
 # response.response
 str(response)
-
+print("1")
 print(response)
 
+time.sleep(2)
